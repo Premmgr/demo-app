3febfd8bd0c3d99498139954faa7c8e5484cb33d
prem mgr <premmgr46@gmail.com>
2023-02-12 15:49:11 +0100

    update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ sudo apt update
 $(which nginx) || sudo apt install nginx -y
 cd /var/www/html && ls | xargs sudo rm -rf && cd
 #sudo mv apache2.conf /etc/apache2
-sudo mv demo-app/app/* /var/www/html
+sudo mv ~/demo-app/app/* /var/www/html
 sudo systemctl restart nginx.service && sudo systemctl status nginx.service | xargs
 } &> provision.logs
